daa24d63fdeb27ad8bba4439dfacac0c7c3e2784
Srinivas Kapa <srinivas.kapa@devrabbit.com>
2023-06-19 10:02:16 +0530

    unneccessary changes

--- eregister/ebanking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from ebanking.views import RegistrationView
-from ebanking.views import CSPViolationView
-
-urlpatterns = [
-    url(r'^eregister/', RegistrationView.as_view()),
-    url(r'^csp-violation-report/', CSPViolationView.as_view()),
-]
